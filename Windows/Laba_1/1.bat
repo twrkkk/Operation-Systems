fb1278cbc84b8fdf74d3b6cc78efa844377506b4
twrkkk <zhenya.shein.03@gmail.com>
2023-03-11 21:41:33 +0300

    .

--- Windows/Laba_1/1.bat
+++ Windows/Laba_1/1.bat
@@ -1,48 +1,12 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 set /a count=0
+set /p dirpath=Enter directory path: 
 
-if "%~1"=="" (
-  echo Usage: %0 directory
-  exit /b 1
+for /d %%i in ("%dirpath%\*") do (
+    set /a count+=1
 )
 
-set "dir=%~1"
-
-if not exist "%dir%" (
-  echo Directory does not exist: %dir%
-  exit /b 1
-)
-
-echo Counting non-hidden subdirectories in "%dir%"...
-
-call :count "%dir%"
-echo Total count: %count%
-exit /b 0
-
-:count
-for /d %%a in ("%~1\*") do (
-  if not "%%~a"=="%~dpnx1\." (
-    if not "%%~a"=="%~dpnx1\.." (
-      if not "%%~a"=="%~dpnx1" (
-        if not "%%~a"=="%~dpnx1\*" (
-          if not "%%~a"=="%~dpnx1\desktop.ini" (
-            set /a count+=1
-          )
-        )
-      )
-    )
-  )
-  call :count "%%~a"
-)
-exit /b 0
-To use this script, save it as a .bat file and run it with the directory name as the only argument. For example:
-
-Copy code
-countdir.bat C:\Users\Username\Documents
-This will count the number of non-hidden subdirectories in the Documents folder of the user "Username" on drive C:.
-
-
-
-
-
+echo %count%
+pause

30e58df610a204f0e15ff18b56e63ed3c3954155
twrkkk <zhenya.shein.03@gmail.com>
2023-03-11 22:13:51 +0300

    ura nahui

--- Windows/Laba_1/2.bat
+++ Windows/Laba_1/2.bat
@@ -1,14 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set /a count=0
 
-for /r "%1" %%d in (*) do (
-    if /i not "%%~ad"=="d" continue
-    set "attr=%%~ad"
-    set "attr=!attr:~1,3!"
-    if /i "!attr!"=="h" continue
-    set /a count+=1
-)
-
-echo The number of non-hidden subdirectories in "%1" is %count%
+set /p dir="Enter the directory path: "
+forfiles /P "%dir%" /M * /C "cmd /c if @fsize GEQ 1048576 echo @path"
+pause
